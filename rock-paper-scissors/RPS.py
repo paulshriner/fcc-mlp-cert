f61d5b11dccca4e076b4996ab69d7cf13f7c6d73
paulshriner <shrinerpaul@gmail.com>
2025-09-29 18:13:10 -0400

    rock-paper-scissors: add functionality

--- rock-paper-scissors/RPS.py
+++ rock-paper-scissors/RPS.py
@@ -1,10 +1,43 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# The winning moves associated with Rock, Paper, Scissors
+winning_moves = {'R': 'P', 'P': 'S', 'S': 'R'}
 
-def player(prev_play, opponent_history=[]):
+# Number of moves we will look back on
+chain_length = 7
+
+# The program works by looking back at a number of the last moves played
+# It then makes potential combinations for each possible move (R, P, S) and sees which one has the highest occurance
+# This will likely be the next move made by the opponent, so we play the countering move
+# Huge thanks to https://medium.com/@sri.hartini/rock-paper-scissors-in-python-5173ab69ca7a for which the idea is based off
+def player(prev_play, opponent_history=[], order={}):
+    # This means we played first, so play as though the opponent's first move is rock
+    if not prev_play:
+        prev_play = "R"
+    
+    # Save opponent's prev move, start by setting prediction to prev move
     opponent_history.append(prev_play)
+    prediction = prev_play
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Need to have minimum of chain_length moves
+    if len(opponent_history) > chain_length - 1:
+        # Player's last moves
+        opp_plays = "".join(opponent_history[-chain_length:])
 
-    return guess
+        # Count how many times set of player's last moves occured
+        order[opp_plays] = order.get(opp_plays, 0) + 1
+        
+        # Get current order of moves based on chain length
+        current_order = "".join(opponent_history[-chain_length + 1::])
+        
+        # Get potential next plays
+        potential_orders = [current_order + i for i in ["R", "P", "S"]]
+        
+        # Determine the order that has occured the most
+        highest = potential_orders[0]
+        for i in range(1, len(potential_orders)):
+            if order.get(potential_orders[i], 0) > order.get(highest, 0):
+                highest = potential_orders[i]
+
+        # Choose move predicted from that order
+        prediction = highest[-1]
+
+    return winning_moves[prediction]
